b727def53c30b8b3bdd65ebfef6e05b179aadcfc
Marcos Vinicius <marcosvinicius08000@gmail.com>
2022-07-07 21:12:45 -0300

    TP2-AEDS

---
+++ tp.d
@@ -0,0 +1,3 @@
+obj/tp.o: \
+ C:\Users\Marcos\Desktop\Segundo-TP-AEDSI-main\Desktop\TP2-AEDS/tp.c \
+ C:\Users\Marcos\Desktop\Segundo-TP-AEDSI-main\Desktop\TP2-AEDS/tp.h
---
+++ tpmain.d
@@ -0,0 +1,3 @@
+obj/tpmain.o: \
+ C:\Users\Marcos\Desktop\Segundo-TP-AEDSI-main\Desktop\TP2-AEDS/tpmain.c \
+ C:\Users\Marcos\Desktop\Segundo-TP-AEDSI-main\Desktop\TP2-AEDS/tp.h
